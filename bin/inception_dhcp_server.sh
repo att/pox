b75fa27fdff67598f1ef0095caf9310f55694e19
Changbin Liu <changbin.liu@gmail.com>
2013-08-06 22:47:55 -0400

    Add a framework for DHCP and according scripts

---
+++ bin/inception_dhcp_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BRIDGE_NAME=obr1
+DHCP_PRIORITY=20
+
+# Intercepts DHCP packets and send them to the controller
+sudo ovs-ofctl add-flow $BRIDGE_NAME "table=0, dl_type=0x0800, \
+  nw_proto=17, tp_src=67, priority=$DHCP_PRIORITY, actions=controller"
